e7742d467a7333bf16550e7968261b3d5373216f
Anchen <li.anchen.au@gmail.com>
2023-04-06 17:42:02 +1000

    fix: fix bug for array as root element

--- src/generateTypeScriptTypes.test.ts
+++ src/generateTypeScriptTypes.test.ts
@@ -3,11 +3,11 @@ import { generateTypeScriptTypes } from "./generateTypeScriptTypes";
 describe("generateTypeScriptTypes", () => {
   test("should generate types for simple JSON", () => {
     const json = `{
-      "name": "John",
-      "age": 30
-    }`;
+        "name": "John",
+        "age": 30
+      }`;
 
-    const expected = `interface Root {
+    const expected = `interface JsonDataType {
   name: string;
   age: number;
 }`;
@@ -17,19 +17,19 @@ describe("generateTypeScriptTypes", () => {
 
   test("should generate types for nested objects", () => {
     const json = `{
-      "user": {
-        "name": "John",
-        "age": 30
-      }
-    }`;
+        "user": {
+          "name": "John",
+          "age": 30
+        }
+      }`;
 
-    const expected = `interface UserRoot {
+    const expected = `interface UserJsonDataType {
   name: string;
   age: number;
 }
 
-interface Root {
-  user: UserRoot;
+interface JsonDataType {
+  user: UserJsonDataType;
 }`;
 
     expect(generateTypeScriptTypes(json)).toBe(expected);
@@ -37,17 +37,48 @@ interface Root {
 
   test("should generate union types for arrays with different element types", () => {
     const json = `{
-      "values": [1, "two", true, { "key": "value" }]
-    }`;
+        "values": [1, "two", true, { "key": "value" }]
+      }`;
 
-    const expected = `interface ValuesRoot {
+    const expected = `interface ValuesJsonDataType3 {
   key: string;
 }
 
-interface Root {
-  values: number | string | boolean | ValuesRoot[];
+interface JsonDataType {
+  values: number | string | boolean | ValuesJsonDataType3[];
 }`;
 
     expect(generateTypeScriptTypes(json)).toBe(expected);
+  });
+
+  it("should handle root level arrays", () => {
+    const json = `[{"id": 1, "name": "John"}, {"id": 2, "name": "Jane"}]`;
+    const expectedResult = `interface RootElement0 {
+  id: number;
+  name: string;
+}
+
+type JsonDataType = RootElement0[];`;
+
+    const result = generateTypeScriptTypes(json);
+    expect(result).toEqual(expectedResult);
+  });
+
+  it("should handle root level arrays with different element types", () => {
+    const json = `[{"id": 1, "name": "John"}, {"age": 30, "city": "New York"}]`;
+    const expectedResult = `interface RootElement0 {
+  id: number;
+  name: string;
+}
+
+interface RootElement1 {
+  age: number;
+  city: string;
+}
+
+type JsonDataType = RootElement0 | RootElement1[];`;
+
+    const result = generateTypeScriptTypes(json);
+    expect(result).toEqual(expectedResult);
   });
 });
--- src/generateTypeScriptTypes.ts
+++ src/generateTypeScriptTypes.ts
@@ -9,14 +9,47 @@ function getType(value: any): string {
   return typeof value;
 }
 
+function getObjectSignature(obj: Record<string, any>): string {
+  return Object.keys(obj)
+    .sort()
+    .map((key) => {
+      const valueType = getType(obj[key]);
+      if (valueType === "object") {
+        return `${key}:{${getObjectSignature(obj[key])}}`;
+      } else if (valueType === "array") {
+        const signatures = new Set<string>();
+        obj[key].forEach((element: any) => {
+          const elementType = getType(element);
+          if (elementType === "object") {
+            signatures.add(`{${getObjectSignature(element)}}`);
+          } else {
+            signatures.add(elementType);
+          }
+        });
+
+        return `${key}:[${Array.from(signatures).join(" | ")}]`;
+      } else {
+        return `${key}:${valueType}`;
+      }
+    })
+    .join(",");
+}
+
 export function generateTypeScriptTypes(
   json: string,
-  rootTypeName: string = "Root"
+  rootTypeName: string = "JsonDataType"
 ): string {
   const data = JSON.parse(json);
   const types: string[] = [];
 
+  const typeCache = new Map<string, string>();
+
   function processObject(obj: Record<string, any>, typeName: string): string {
+    const signature = getObjectSignature(obj);
+    if (typeCache.has(signature)) {
+      return typeCache.get(signature)!;
+    }
+
     let typeDef = `interface ${typeName} {\n`;
     for (const key in obj) {
       const valueType = getType(obj[key]);
@@ -42,14 +75,19 @@ export function generateTypeScriptTypes(
       typeDef += `  ${fieldName}: ${fieldType};\n`;
     }
     typeDef += "}";
-    types.push(typeDef);
+
+    if (!types.some((t) => t === typeDef)) {
+      types.push(typeDef);
+    }
+
+    typeCache.set(signature, typeName);
     return typeName;
   }
 
   function processArray(arr: any[], typeName: string): string {
     const elementTypes = new Set<string>();
 
-    arr.forEach((element) => {
+    arr.forEach((element, index) => {
       const valueType = getType(element);
       let elementType = "";
 
@@ -60,10 +98,11 @@ export function generateTypeScriptTypes(
           elementType = valueType;
           break;
         case "object":
-          elementType = processObject(element, typeName);
+          const objectTypeName = `${typeName}${index}`;
+          elementType = processObject(element, objectTypeName);
           break;
         case "array":
-          const nestedArrayTypeName = typeName + "Item";
+          const nestedArrayTypeName = `${typeName}${index}Item`;
           elementType = processArray(element, nestedArrayTypeName);
           break;
       }
@@ -75,7 +114,11 @@ export function generateTypeScriptTypes(
     return `${unionType}[]`;
   }
 
-  processObject(data, rootTypeName);
+  if (Array.isArray(data)) {
+    types.push(`type ${rootTypeName} = ${processArray(data, "RootElement")};`);
+  } else {
+    processObject(data, rootTypeName);
+  }
 
   return types.join("\n\n");
 }
